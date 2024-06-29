c0cfda0db1be141fe7c33bebce8168bbbd1b9d4a
Nelson Neto <nelson.ash@outlook.com>
2024-06-29 16:24:24 -0300

    Criar OpenApi com Swagger

---
+++ Athena.Minimal/Program.cs
@@ -0,0 +1,66 @@
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container. Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", (string? email) =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi(GenerateOperation());
+
+static Func<OpenApiOperation, OpenApiOperation> GenerateOperation()
+{
+    return operation =>
+    {
+        // Summary
+        operation.Summary = "Endpoint used to fetch users";
+
+        // Parameters
+        var parameter = operation.Parameters[0];
+        parameter.Description = "The user e-mail. It will filter the users by using this value";
+        parameter.Required = false;
+        parameter.AllowEmptyValue = true;
+        parameter.Examples = new Dictionary<string, OpenApiExample>()
+        {
+            { "talles.valiatt@tallesvaliatti.com", new OpenApiExample { Value = new OpenApiString("talles.valiatt@tallesvaliatti.com"),Description = "It will filter users who have their email as 'talles.valatt@tallesvaliatti.com'" } },
+            { "talles", new OpenApiExample { Value = new OpenApiString("talles"),Description = "It will filter users if their email contains the word 'talles'" } }
+        };
+        return operation;
+    };
+}
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
